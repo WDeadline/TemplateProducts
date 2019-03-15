68e89999834f385a9cbd2f8b094af8158382fe67
Tri Doan Quoc <dqtri@tma.com.vn>
2019-03-15 19:45:44 +0700

    Create model User

---
+++ TemplateSamples/TemplateSamples/Models/Action.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemplateSamples.Models
+{
+    public class Action
+    {
+    }
+}
---
+++ TemplateSamples/TemplateSamples/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemplateSamples.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public byte[] Salt { get; set; }
+
+        public byte[] Password { get; set; }
+
+        public bool Active { get; set; } = true;
+
+        public IEnumerable<Permission> Roles { get; set; }
+    }
+}
