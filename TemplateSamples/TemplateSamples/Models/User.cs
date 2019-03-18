fc1b5960f602f1e97f3a91279bef0b0c0e5fc95c
Tri Doan Quoc <dqtri@tma.com.vn>
2019-03-18 19:44:25 +0700

    Update User.cs Add some field

--- TemplateSamples/TemplateSamples/Models/User.cs
+++ TemplateSamples/TemplateSamples/Models/User.cs
@@ -9,22 +9,35 @@ namespace TemplateSamples.Models
     {
         public long Id { get; set; }
 
+        public string UserName { get; set; }
+
+        public string NormalizedUserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string NormalizedEmail { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
-        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public bool PhoneNumberConfirmed { get; set; }
 
         public string Address { get; set; }
 
-        public string EmailAddress { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
-        public byte[] Salt { get; set; }
+        public byte[] PasswordHash { get; set; }
 
-        public byte[] Password { get; set; }
+        public int AccessFailedCount { get; set; }
 
-        public bool Active { get; set; } = true;
+        public bool LockoutEnabled { get; set; }
 
-        public IEnumerable<Permission> Roles { get; set; }
+        public IEnumerable<Permission> Permissions { get; set; }
+
     }
 }
